ba71c5e80049e3c9f3ed6d880a37915a38baa72b
carlosportella16 <carlosportella16@hotmail.com>
2025-06-22 12:33:09 -0300

    feat: SecurityConfig for new route

--- src/main/java/encurtador/config/SecurityConfig.java
+++ src/main/java/encurtador/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/oauth/token").permitAll()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
